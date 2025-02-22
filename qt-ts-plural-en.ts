4a1f71f209cf9ab2fbaa1ac3950b1c41218010d1
dev-25 <vivid.kevaldobariya@gmail.com>
2025-02-22 16:48:21 +0530

    added sample files

---
+++ qt-ts-plural-en.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8" ?>
+
+<TS version="2.0" language="en_US">
+  <context>
+    <name>Dialog</name>
+    <message numerus="yes">
+      <location filename="dialog.ui" line="16"/>
+      <extracomment>Sample comment for translator</extracomment>
+      <translation/>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message numerus="yes">
+      <location filename="mainwindow.ui" line="14"/>
+      <location filename="mainwindow.ui" line="31"/>
+      <source>Qt-localization-test</source>
+      <translation>
+        <numerusform>Book</numerusform>
+        <numerusform>Books</numerusform>
+      </translation>
+    </message>
+    <message numerus="yes">
+      <location filename="mainwindow.ui" line="47"/>
+      <source>Kieli</source>
+      <extracomment>Another comment for translator</extracomment>
+      <translation>
+        <numerusform>Test</numerusform>
+        <numerusform>Tests</numerusform>
+      </translation>
+    </message>
+  </context>
+</TS>
