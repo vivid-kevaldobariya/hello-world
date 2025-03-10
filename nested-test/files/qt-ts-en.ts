37c94ebf9c1d84b6ff1b23b8614a75bdbd0262bc
dev-25 <vivid.kevaldobariya@gmail.com>
2025-03-10 17:36:16 +0530

    Add files via upload

---
+++ nested-test/files/qt-ts-en.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8" ?>
+
+<TS version="2.0" language="en_US">
+  <context>
+    <name>Dialog</name>
+    <message>
+      <location filename="dialog.ui" line="16"/>
+      <extracomment>Sample comment for translator</extracomment>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="mainwindow.ui" line="14"/>
+      <location filename="mainwindow.ui" line="31"/>
+      <source>Qt-localization-test</source>
+      <translation>Test</translation>
+    </message>
+    <message>
+      <location filename="mainwindow.ui" line="47"/>
+      <source>Kieli</source>
+      <extracomment>Another comment for translator</extracomment>
+      <translation>Language</translation>
+    </message>
+  </context>
+</TS>
